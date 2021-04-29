7931da892005a553119cf37c784dc8d9028298e6
byronlin13 <57543366+byronlin13@users.noreply.github.com>
2021-04-28 23:45:26 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+import re
+import setuptools
+
+requires = ['amazon.ion>=0.5.0,<1]
+
+setuptools.setup(
+    name='pythonplayground',
+    version='1.0',
+    description='description',
+    long_description='long_description',
+    long_description_content_type='text/markdown',
+    author='byron',
+    packages=setuptools.find_packages(),
+    install_requires=requires,
+    license="Apache License 2.0"
+)
